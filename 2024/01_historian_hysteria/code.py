1a77216c5277b060395f40b329f35f96b3b278a7
montanarograziano <graziano.montanaro98@gmail.com>
2024-12-03 11:13:34 +0100

    Day 1 2024 solution

---
+++ 2024/01_historian_hysteria/code.py
@@ -0,0 +1,42 @@
+"""AoC 1, 2024."""
+
+# Standard library imports
+import pathlib
+import sys
+
+INPUT_FILE = "input.txt"
+
+
+def parse_data(puzzle_input: str):
+    """Parse input."""
+    return [list(map(int, x.split())) for x in puzzle_input.splitlines()]
+
+
+def part1(data):
+    """Solve part 1."""
+    left, right = [list(x) for x in zip(*data)]
+    return sum(abs(x - y) for x, y in zip(sorted(left), sorted(right)))
+
+
+def part2(data):
+    """Solve part 2."""
+    left, right = [list(x) for x in zip(*data)]
+    return sum(x * right.count(x) for x in left)
+
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input."""
+    data = parse_data(puzzle_input)
+    yield part1(data)
+    yield part2(data)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        path = sys.argv[1]
+        print(f"\n{path}:")
+        solutions = solve(puzzle_input=pathlib.Path(path).read_text().rstrip())
+        print("\n".join(str(solution) for solution in solutions))
+    else:
+        solutions = solve(puzzle_input=pathlib.Path(INPUT_FILE).read_text().rstrip())
+        print("\n".join(str(solution) for solution in solutions))
